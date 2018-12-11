e3a8dd87995725eab979368e482f7574883b0408
hillrg22 <hillrg22@gmail.com>
2018-12-11 11:43:07 -0700

    built some basic classes and subclasses with constructors and methods

--- classes.js
+++ classes.js
@@ -1,9 +1,45 @@
 // Create custom data structures
 // 1. Code-along here
 
+class Spell {
+  constructor(name, unforgivable, type, difficulty){
+    this.name = name
+    this.unforgivable = unforgivable
+    this.type = type
+    this.difficulty = difficulty
+  }
+
+  setDifficulty(newDifficulty) {
+    this.difficulty = newDifficulty
+  }
+
+  getDifficulty(){
+    return this.difficulty
+  }
+
+}
+
+let Patronus = new Spell("Patronus", false, "Defense", 7)
+
+let Expelliarmus = new Spell("Expelliarmus", false, "Attack", 5)
+
+Expelliarmus.setDifficulty(6)
+
+console.log(Expelliarmus)
+
+
+class AttackSpell extends Spell {
+  constructor(name, unforgiveable, difficulty, damage){
+    super(name, unforgiveable, "Attack", difficulty, damage)
+    this.damage = damage
+  }
+}
+let crucio = new AttackSpell("Crucio", true, 8, "High")
+console.log(crucio)
 
 
 // 2. Create your own class
+
 
 
 
@@ -11,4 +47,4 @@
 
 
 
-// 4. 
+// 4.
